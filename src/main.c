45c2aec3993a967078b7e3482ef2042411e0b035
Sander Ruben <sanderruben@live.nl>
2016-12-08 01:56:06 +0100

    added test input and added game_settings struct

--- src/main.c
+++ src/main.c
@@ -1,5 +1,20 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define MAX_LINE_LENGTH 255
+
+struct
+{
+  int timebank;
+  int time_per_move;
+  int botid;
+  int field_columns;
+  int field_rows;
+} game_settings;
+
 
 int main(int argc, const char *argv[])
 {
